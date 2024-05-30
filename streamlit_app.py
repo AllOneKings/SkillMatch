34be79b503e1b99fc11c841ac76f67bbae028ee9
raphaellG <100555636+AllOneKings@users.noreply.github.com>
2024-05-30 22:45:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -429,7 +429,7 @@ def run():
                                 android_keyword = ['android', 'android development', 'flutter', 'kotlin', 'xml', 'kivy']
                                 ios_keyword = ['ios', 'ios development', 'swift', 'cocoa', 'cocoa touch', 'xcode']
                                 uiux_keyword = ['ux', 'adobe xd', 'figma', 'zeplin', 'balsamiq', 'ui', 'prototyping', 'wireframes', 'storyframes', 'adobe photoshop', 'photoshop', 'editing', 'adobe illustrator', 'illustrator', 'adobe after effects', 'after effects', 'adobe premier pro', 'premier pro', 'adobe indesign', 'indesign', 'wireframe', 'solid', 'grasp', 'user research', 'user experience']
-                                n_any = ['english','communication','writing', 'microsoft office', 'leadership','customer management', 'social media']
+                                n_any = ['writing', 'leadership','customer management', 'social media']
     
                                 ### Skill Recommendations Starts
                                 recommended_skills = []
